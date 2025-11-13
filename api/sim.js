16afe966ab5cb904174e40d141e28effe649732f
Marcello Máchilas <marcello.machilas@gmail.com>
2025-11-13 13:58:50 -0300

    ajuste envio de email

--- api/sim.js
+++ api/sim.js
@@ -1,28 +1,29 @@
 import nodemailer from "nodemailer";
-
+import dotenv from "dotenv";
+dotenv.config();
 export default async function handler(req, res) {
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.SMTP_USER, // marcello.machilas@gmail.com
-        pass: process.env.SMTP_PASS  // senha de app do Google
-      }
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
     });
 
     await transporter.sendMail({
       from: `"Cartinha ❤️" <${process.env.SMTP_USER}>`,
-      to: "machilaswestside@gmail.com", // <-- email que recebe
+      to: process.env.RECEIVER_EMAIL,
       subject: "Ela respondeu: SIM 💖",
       html: `
         <h2>💖 Ela respondeu SIM!</h2>
         <p>Ela quer recomeçar com você. 🌹✨</p>
-      `
+      `,
     });
 
     return res.status(200).json({ success: true });
-  } catch (err) {
-    console.log("Erro:", err);
+  } catch (error) {
+    console.error("Erro ao enviar e-mail (SIM):", error);
     return res.status(500).json({ error: "Erro ao enviar e-mail" });
   }
 }
